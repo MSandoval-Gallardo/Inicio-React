405c1ffa977d1ebf51d141687ee15413707b73fc
MSandoval-Gallardo <m.sandovalgallardo@gmail.com>
2025-09-30 13:38:17 -0300

    ejemplo .then, .catch y .finally

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import { useState } from "react"
-import Modal from "./components/Modal";
+import { useEffect, useState } from "react";
+
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 const App = () => {
 
-  const [isOpen, setIsOpen] = useState(true)
+  const [todo, setTodo] = useState<Todo | null>(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<null | string>(null)
 
-  return (
-    <div className="container mx-auto">
-      <h1>useEffect</h1>
-      <button 
-      onClick={() => {
-        setIsOpen(!isOpen)}}
-      className="btn btn-primary">
-        {
-          isOpen ? "Cerrar" : "Abrir"
+  useEffect(() => {
 
-        }</button>
-      {
-        isOpen && <Modal/>
+    fetch("https://jsonplaceholder.typicode.com/todos/10000000")
+    .then((res) => {
+      if(res.ok) return res.json();
+        console.log("Algo salió mal");
+        throw new Error("404 en la API");
+    })
+    .then((data: Todo) => {
+      console.log(data)
+      setTodo(data)
+      
+    })
+    .catch((e: unknown) => {
+      if(e instanceof Error){
+       return setError(e.message)
       }
+      console.log(e);
+      setError("error mas grave...")
+    })
+    .finally(() => {
+      setIsLoading(false)
+    });
+  }, [])
+
+  if(isLoading) return <p>Loading...</p>
+  if(error) return <p>{error}</p>
+  return (
+    <div>
+      <h1>Fetch</h1>
+      <h4>Id {todo?.id}</h4>
+      <h2>Titulo {todo?.title}</h2>
+      <h3>Estado {todo?.completed ? "Tarea completada" : "Tarea no completada"}</h3>
     </div>
   )
 }
